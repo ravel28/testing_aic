4057887df6d10034a59110072b41c0d7b3746deb
ravel28 <ravelinno9@gmail.com>
2025-03-27 10:09:16 +0700

    basic-project

---
+++ database/migrations/2025_03_27_023624_tugas.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tugas', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('jumlah_jam_kerja');
+            $table->integer('tarif');
+            $table->integer('tarif_tambahan');
+            $table->timestamps();
+        });
+
+        Schema::create('tugas_per_pekerja', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('jam_mulai');
+            $table->timestamp('jam_selesai');
+            $table->timestamps();
+            $table->foreignId('pegawai_id')->constrained('pegawai')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tugas');
+        Schema::dropIfExists('tugas_per_pekerja');
+    }
+};
